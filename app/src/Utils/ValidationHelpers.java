891cbe04c3613a6e0388283a8487332d5f002254
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-05-11 16:31:00 +0300

    Fixed an error in the names of the methods

--- app/src/Utils/ValidationHelpers.java
+++ app/src/Utils/ValidationHelpers.java
@@ -1,12 +1,12 @@
 package Utils;
 
 public class ValidationHelpers {
-        public static void ValidateIntRange(int input, int min, int max, String errorMessage) {
+        public static void validateIntRange(int input, int min, int max, String errorMessage) {
                 if (input < min || input > max) {
                         throw new IllegalArgumentException(errorMessage);
                 }
         }
-        public static void ValidateStringLength(String input, int min, int max, String errorMessage) {
-                ValidateIntRange(input.length(), min, max, errorMessage);
+        public static void validateStringLength(String input, int min, int max, String errorMessage) {
+                validateIntRange(input.length(), min, max, errorMessage);
         }
 }
