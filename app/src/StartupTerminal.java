741ef3ade5563c305b40a79c1842bebb10bda4ec
svetoslavst <svetoslav.stoyanov66@gmail.com>
2024-05-11 15:45:31 +0300

    main packages

---
+++ app/src/StartupTerminal.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.IOException;
+
+public class StartupTerminal {
+    static String javaHome = System.getProperty("java.home");
+    static String javacPath = javaHome + File.separator + "bin" + File.separator + "javac";
+
+    public static void main(String[] args) {
+        try {
+
+            String jarPath = "out/artifacts/app_jar/app.jar";
+
+            String osName = System.getProperty("os.name").toLowerCase();
+            ProcessBuilder runBuilder = new ProcessBuilder();
+
+            if (osName.contains("win")) {
+                runBuilder = new ProcessBuilder( "cmd.exe", "/k", "java", "-jar " + jarPath);
+            } else if (osName.contains("linux")) {
+                runBuilder.command("xfce4-terminal", "--command", "java -jar " + jarPath);
+            } else if (osName.contains("mac")) {
+                runBuilder = new ProcessBuilder();
+            } else {
+                System.err.println("Unsupported operating system: " + osName);
+                System.exit(1); // Exit with error code
+            }
+
+
+            runBuilder.start();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
