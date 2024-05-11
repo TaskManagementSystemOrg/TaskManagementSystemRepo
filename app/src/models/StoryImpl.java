f4c4e6f64ed0cc027c0f8184e9ba9cf9abeb0fe9
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-05-12 00:46:08 +0300

    Implemented basic functionality for Story class

--- app/src/models/StoryImpl.java
+++ app/src/models/StoryImpl.java
@@ -1,4 +1,52 @@
 package models;
 
-public class StoryImpl {
+import models.contracts.Person;
+import models.contracts.Story;
+import models.enums.Priority;
+import models.enums.Size;
+import models.enums.StoryStatus;
+
+public class StoryImpl extends TaskImpl implements Story {
+    private Priority priority;
+    private Size size;
+    private StoryStatus status;
+    private Person assignee;
+
+    public StoryImpl(int id, String title, String description, Priority priority, Size size, StoryStatus status, Person assignee) {
+        super(id, title, description);
+        setPriority(priority);
+        setSize(size);
+        setStatus(status);
+        setAssignee(assignee);
+    }
+
+    private void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+    private void setSize(Size size) {
+        this.size = size;
+    }
+    private void setStatus(StoryStatus status) {
+        this.status = status;
+    }
+    private void setAssignee(Person assignee) {
+        this.assignee = assignee;
+    }
+
+    @Override
+    public Priority getPriority() {
+        return this.priority;
+    }
+    @Override
+    public Size getSize() {
+        return this.size;
+    }
+    @Override
+    public StoryStatus getStatus() {
+        return this.status;
+    }
+    @Override
+    public Person getAssignee() {
+        return this.assignee;
+    }
 }
