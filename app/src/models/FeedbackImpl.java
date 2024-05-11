c18af22a25c0580c6300c52cfac9e402383e9048
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-05-12 00:51:08 +0300

    Implemented basic functionality for Feedback Class

--- app/src/models/FeedbackImpl.java
+++ app/src/models/FeedbackImpl.java
@@ -1,4 +1,31 @@
 package models;
 
-public class FeedbackImpl {
+import models.contracts.Feedback;
+import models.enums.FeedbackStatus;
+
+public class FeedbackImpl extends TaskImpl implements Feedback {
+    private int rating;
+    private FeedbackStatus status;
+
+    public FeedbackImpl(int id, String title, String description, int rating, FeedbackStatus status) {
+        super(id, title, description);
+        setRating(rating);
+        setStatus(status);
+    }
+
+    private void setRating(int rating) {
+        this.rating = rating;
+    }
+    private void setStatus(FeedbackStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public int getRating() {
+        return rating;
+    }
+    @Override
+    public FeedbackStatus getStatus() {
+        return status;
+    }
 }
