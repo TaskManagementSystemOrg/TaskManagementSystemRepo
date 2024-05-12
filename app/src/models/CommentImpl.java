0709c140fc1bf449b28c0e8e234d46fbd80d23ad
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-05-12 13:47:49 +0300

    Added commentImpl basic functionality

--- app/src/models/CommentImpl.java
+++ app/src/models/CommentImpl.java
@@ -1,4 +1,27 @@
 package models;
 
+import models.contracts.Person;
+
 public class CommentImpl {
+    private Person author;
+    private String content;
+
+    public CommentImpl(Person author, String content) {
+        setAuthor(author);
+        setContent(content);
+    }
+
+    private void setAuthor(Person author) {
+        this.author = author;
+    }
+    private void setContent(String content) {
+        this.content = content;
+    }
+
+    public Person getAuthor() {
+        return author;
+    }
+    public String getContent() {
+        return content;
+    }
 }
