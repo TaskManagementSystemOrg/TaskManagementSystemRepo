d67331373c2155c003d125c439b8cd7432983576
svetoslavst <svetoslav.stoyanov66@gmail.com>
2024-05-20 17:12:41 +0300

    reworked factoryImpl

--- app/src/core/CommandFactoryImpl.java
+++ app/src/core/CommandFactoryImpl.java
@@ -37,121 +37,160 @@ public class CommandFactoryImpl implements CommandFactory {
 
         switch (taskManagementSystemRepository.getCurrentUser()) {
             case NOT_LOGGED_IN:
-                switch (commandType) {
-                    case LOG_IN:
-                        return new LogInCommand(taskManagementSystemRepository);
-                    default:
-                        return new NotLoggedInCommand();
-                }
-            case NORMAL:
-                if (taskManagementSystemRepository.getCurrentBoard() != null) {
-                    switch (commandType) {
-                        case SHOW_BOARD_ACTIVITY:
-                            return new ShowBoardActivityCommand(taskManagementSystemRepository);
-                        case CREATE_BUG:
-                            return new CreateNewBugCommand(taskManagementSystemRepository);
-                        case CREATE_STORY:
-                            return new CreateNewStoryCommand(taskManagementSystemRepository);
-                        case CREATE_FEEDBACK:
-                            return new CreateNewFeedbackCommand(taskManagementSystemRepository);
-                        case EXIT_BOARD:
-                            return new ExitBoardCommand(taskManagementSystemRepository);
-                        case CREATE_BOARD:
-                            return new CreateNewBoardInATeam(taskManagementSystemRepository);
-                        case LOG_OUT:
-                            return new LogOutCommand(taskManagementSystemRepository);
-                        default:
-                            throw new IllegalArgumentException();
+                return handleNotLoggedInCommands(commandType, taskManagementSystemRepository);
 
-                    }
-                } else
-                    switch (commandType) {
-                        case CREATE_PERSON:
-                            return new CreateNewPersonCommand(taskManagementSystemRepository);
-                        case SHOW_PERSON_ACTIVITY:
-                            return new ShowPersonsActivityCommand(taskManagementSystemRepository);
-                        case CREATE_TEAM:
-                            return new CreateNewTeamCommand(taskManagementSystemRepository);
-                        case SHOW_TEAM_ACTIVITY:
-                            return new ShowTeamsActivityCommand(taskManagementSystemRepository);
-                        case ADD_PERSON_TO_TEAM:
-                            return new AddPersonToTeamCommand(taskManagementSystemRepository);
-                        case SHOW_ALL_TEAM_MEMBERS:
-                            return new ShowAllTeamMembers(taskManagementSystemRepository);
-                        case CREATE_BOARD:
-                            return new CreateNewBoardInATeam(taskManagementSystemRepository);
-                        case SHOW_ALL_TEAM_BOARDS:
-                            return new ShowAllTeamBoardsCommand(taskManagementSystemRepository);
-                        case SHOW_BOARD_ACTIVITY:
-                            return new ShowBoardActivityCommand(taskManagementSystemRepository);
-                        case CREATE_BUG:
-                            return new CreateNewBugCommand(taskManagementSystemRepository);
-                        case CREATE_STORY:
-                            return new CreateNewStoryCommand(taskManagementSystemRepository);
-                        case CREATE_FEEDBACK:
-                            return new CreateNewFeedbackCommand(taskManagementSystemRepository);
-                        case CHANGE_BUG_PRIORITY:
-                            return new ChangeBugPriority(taskManagementSystemRepository);
-                        case CHANGE_BUG_SEVERITY:
-                            return new ChangeBugSeverity(taskManagementSystemRepository);
-                        case CHANGE_BUG_STATUS:
-                            return new ChangeBugStatus(taskManagementSystemRepository);
-                        case CHANGE_STORY_PRIORITY:
-                            return new ChangeStoryPriority(taskManagementSystemRepository);
-                        case CHANGE_STORY_SIZE:
-                            return new ChangeStorySize(taskManagementSystemRepository);
-                        case CHANGE_STORY_STATUS:
-                            return new ChangeStoryStatus(taskManagementSystemRepository);
-                        case CHANGE_FEEDBACK_RATING:
-                            return new ChangeFeedbackRating(taskManagementSystemRepository);
-                        case CHANGE_FEEDBACK_STATUS:
-                            return new ChangeFeedbackStatus(taskManagementSystemRepository);
-                        case ASSIGN_TASK:
-                            return new AssignTaskToAPerson(taskManagementSystemRepository);
-                        case UNASSIGN_TASK:
-                            return new UnassignTaskToAPerson(taskManagementSystemRepository);
-                        case ENTER_BOARD:
-                            return new EnterBoardCommand(taskManagementSystemRepository);
-                        case ENTER_TEAM:
-                            return new EnterTeamCommand(taskManagementSystemRepository);
-                        case EXIT_TEAM:
-                            return new ExitTeamCommand(taskManagementSystemRepository);
-                        case SAVE:
-                            return new SaveCommand(taskManagementSystemRepository);
-                        case LOG_OUT:
-                            return new LogOutCommand(taskManagementSystemRepository);
-                        default:
-                            throw new IllegalArgumentException();
-                    }
+            case NORMAL:
+                return handleNormalUserCommands(commandType, taskManagementSystemRepository);
+
             case ADMIN:
-                switch (commandType) {
-                    case SHOW_ALL_PEOPLE:
-                        return new ShowAllPeopleCommand(taskManagementSystemRepository);
-                    case SHOW_ALL_TEAMS:
-                        return null;
-                    case SHOW_ALL_BUGS:
-                        return new ShowAllBugsCommand(taskManagementSystemRepository);
-                    case SHOW_ALL_FEEDBACK:
-                        return new ShowAllFeedbackCommand(taskManagementSystemRepository);
-                    case SHOW_ALL_STORIES:
-                        return new ShowAllStoriesCommand(taskManagementSystemRepository);
-                    case SHOW_ALL_TASKS:
-                        return new ShowAllTasksCommand(taskManagementSystemRepository);
-                    case SHOW_ALL_TASKS_WITH_ASSIGNEE:
-                        return new ShowTasksWithAssigneeCommand(taskManagementSystemRepository);
-                    case LOG_OUT:
-                        return new LogOutCommand(taskManagementSystemRepository);
-                    default:
-                        throw new IllegalArgumentException();
-                }
+                return handleAdminUserCommands(commandType, taskManagementSystemRepository);
+
+            default:
+                throw new IllegalArgumentException(String.format(INVALID_COMMAND, commandTypeAsString));
+        }
+    }
+
+    private Command handleNotLoggedInCommands(CommandType commandType, TaskManagementSystemRepository taskManagementSystemRepository) {
+        switch (commandType) {
+            case LOG_IN:
+                return new LogInCommand(taskManagementSystemRepository);
+            default:
+                return new NotLoggedInCommand();
+        }
+    }
+
+    private Command handleNormalUserCommands(CommandType commandType, TaskManagementSystemRepository taskManagementSystemRepository) {
+        if (taskManagementSystemRepository.getCurrentBoard() != null) {
+            return handleNormalUserWithBoardCommands(commandType, taskManagementSystemRepository);
+        } else {
+            return handleNormalUserWithoutBoardCommands(commandType, taskManagementSystemRepository);
+        }
+    }
+
+    private Command handleNormalUserWithBoardCommands(CommandType commandType, TaskManagementSystemRepository taskManagementSystemRepository) {
+        switch (commandType) {
+            case SHOW_BOARD_ACTIVITY:
+                return new ShowBoardActivityCommand(taskManagementSystemRepository);
+            case CREATE_BUG:
+                return new CreateNewBugCommand(taskManagementSystemRepository);
+            case CREATE_STORY:
+                return new CreateNewStoryCommand(taskManagementSystemRepository);
+            case CREATE_FEEDBACK:
+                return new CreateNewFeedbackCommand(taskManagementSystemRepository);
+            case EXIT_BOARD:
+                return new ExitBoardCommand(taskManagementSystemRepository);
+            case LOG_OUT:
+                return new LogOutCommand(taskManagementSystemRepository);
             default:
                 throw new IllegalArgumentException();
         }
     }
+
+    private Command handleNormalUserWithoutBoardCommands(CommandType commandType, TaskManagementSystemRepository taskManagementSystemRepository) {
+        switch (commandType) {
+            case SHOW_PERSON_ACTIVITY:
+                return new ShowPersonsActivityCommand(taskManagementSystemRepository);
+            case SHOW_TEAM_ACTIVITY:
+                return new ShowTeamsActivityCommand(taskManagementSystemRepository);
+            case SHOW_ALL_TEAM_MEMBERS:
+                return new ShowAllTeamMembers(taskManagementSystemRepository);
+            case SHOW_ALL_TEAM_BOARDS:
+                return new ShowAllTeamBoardsCommand(taskManagementSystemRepository);
+            case ENTER_BOARD:
+                return new EnterBoardCommand(taskManagementSystemRepository);
+            case ENTER_TEAM:
+                return new EnterTeamCommand(taskManagementSystemRepository);
+            case EXIT_TEAM:
+                return new ExitTeamCommand(taskManagementSystemRepository);
+            case EXIT_BOARD:
+                return new ExitBoardCommand(taskManagementSystemRepository);
+            case SAVE:
+                return new SaveCommand(taskManagementSystemRepository);
+            case LOG_OUT:
+                return new LogOutCommand(taskManagementSystemRepository);
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
+    private Command handleAdminUserCommands(CommandType commandType, TaskManagementSystemRepository taskManagementSystemRepository) {
+        switch (commandType) {
+            case SHOW_ALL_PEOPLE:
+                return new ShowAllPeopleCommand(taskManagementSystemRepository);
+            case SHOW_ALL_TEAMS:
+                return new ShowAllTeamsCommand(taskManagementSystemRepository);
+            case SHOW_ALL_BUGS:
+                return new ShowAllBugsCommand(taskManagementSystemRepository);
+            case SHOW_ALL_FEEDBACK:
+                return new ShowAllFeedbackCommand(taskManagementSystemRepository);
+            case SHOW_ALL_STORIES:
+                return new ShowAllStoriesCommand(taskManagementSystemRepository);
+            case SHOW_ALL_TASKS:
+                return new ShowAllTasksCommand(taskManagementSystemRepository);
+            case SHOW_ALL_TASKS_WITH_ASSIGNEE:
+                return new ShowTasksWithAssigneeCommand(taskManagementSystemRepository);
+            case CREATE_PERSON:
+                return new CreateNewPersonCommand(taskManagementSystemRepository);
+            case SHOW_PERSON_ACTIVITY:
+                return new ShowPersonsActivityCommand(taskManagementSystemRepository);
+            case CREATE_TEAM:
+                return new CreateNewTeamCommand(taskManagementSystemRepository);
+            case SHOW_TEAM_ACTIVITY:
+                return new ShowTeamsActivityCommand(taskManagementSystemRepository);
+            case ADD_PERSON_TO_TEAM:
+                return new AddPersonToTeamCommand(taskManagementSystemRepository);
+            case SHOW_ALL_TEAM_MEMBERS:
+                return new ShowAllTeamMembers(taskManagementSystemRepository);
+            case CREATE_BOARD:
+                return new CreateNewBoardInATeam(taskManagementSystemRepository);
+            case SHOW_ALL_TEAM_BOARDS:
+                return new ShowAllTeamBoardsCommand(taskManagementSystemRepository);
+            case CREATE_BUG:
+                return new CreateNewBugCommand(taskManagementSystemRepository);
+            case CREATE_STORY:
+                return new CreateNewStoryCommand(taskManagementSystemRepository);
+            case CREATE_FEEDBACK:
+                return new CreateNewFeedbackCommand(taskManagementSystemRepository);
+            case CHANGE_BUG_PRIORITY:
+                return new ChangeBugPriority(taskManagementSystemRepository);
+            case CHANGE_BUG_SEVERITY:
+                return new ChangeBugSeverity(taskManagementSystemRepository);
+            case CHANGE_BUG_STATUS:
+                return new ChangeBugStatus(taskManagementSystemRepository);
+            case CHANGE_STORY_PRIORITY:
+                return new ChangeStoryPriority(taskManagementSystemRepository);
+            case CHANGE_STORY_SIZE:
+                return new ChangeStorySize(taskManagementSystemRepository);
+            case CHANGE_STORY_STATUS:
+                return new ChangeStoryStatus(taskManagementSystemRepository);
+            case CHANGE_FEEDBACK_RATING:
+                return new ChangeFeedbackRating(taskManagementSystemRepository);
+            case CHANGE_FEEDBACK_STATUS:
+                return new ChangeFeedbackStatus(taskManagementSystemRepository);
+            case ASSIGN_TASK:
+                return new AssignTaskToAPerson(taskManagementSystemRepository);
+            case UNASSIGN_TASK:
+                return new UnassignTaskToAPerson(taskManagementSystemRepository);
+            case ENTER_BOARD:
+                return new EnterBoardCommand(taskManagementSystemRepository);
+            case ENTER_TEAM:
+                return new EnterTeamCommand(taskManagementSystemRepository);
+            case EXIT_BOARD:
+                return new ExitBoardCommand(taskManagementSystemRepository);
+            case EXIT_TEAM:
+                return new ExitTeamCommand(taskManagementSystemRepository);
+            case SAVE:
+                return new SaveCommand(taskManagementSystemRepository);
+            case LOG_OUT:
+                return new LogOutCommand(taskManagementSystemRepository);
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
     public static void clearConsole() {
         try {
             String os = System.getProperty("os.name");
-
             if (os.contains("Windows")) {
                 new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
             } else {
