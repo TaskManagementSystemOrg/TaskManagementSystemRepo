ef2fd6575ec9ee345baa3c940e3defa5debbc240
svetoslavst <svetoslav.stoyanov66@gmail.com>
2024-05-22 14:00:54 +0300

    board serialization addition

--- app/src/models/deserializers/BoardDeserializer.java
+++ app/src/models/deserializers/BoardDeserializer.java
@@ -1,4 +1,39 @@
 package models.deserializers;
 
-public class BoardDeserializer {
+import com.google.gson.*;
+import models.BoardImpl;
+import models.contracts.Task;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoardDeserializer implements JsonDeserializer<BoardImpl> {
+
+    @Override
+    public BoardImpl deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject jsonObject = json.getAsJsonObject();
+
+        String name = jsonObject.get("name").getAsString();
+        BoardImpl board = new BoardImpl(name);
+
+        JsonArray activityArray = jsonObject.getAsJsonArray("activityHistory");
+        List<String> activityHistory = new ArrayList<>();
+        for (JsonElement activityElement : activityArray) {
+            String activity = activityElement.getAsString();
+            activityHistory.add(activity);
+        }
+        board.getActivity().addAll(activityHistory);
+
+        // Deserialize tasks
+        JsonArray tasksArray = jsonObject.getAsJsonArray("tasks");
+        List<Integer> tasks = new ArrayList<>();
+        for (JsonElement taskElement : tasksArray) {
+            int taskId = taskElement.getAsInt();
+            tasks.add(taskId);
+        }
+        board.getTasks().addAll(tasks);
+
+        return board;
+    }
 }
--- app/src/models/serializers/BoardSerializer.java
+++ app/src/models/serializers/BoardSerializer.java
@@ -1,4 +1,29 @@
 package models.serializers;
 
-public class BoardSerializer {
+import com.google.gson.*;
+import models.BoardImpl;
+
+import java.lang.reflect.Type;
+
+public class BoardSerializer implements JsonSerializer<BoardImpl> {
+    @Override
+    public JsonElement serialize(BoardImpl board, Type type, JsonSerializationContext jsonSerializationContext) {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("class", board.getClass().getName());
+        jsonObject.addProperty("name", board.getName());
+
+        JsonArray activityArray = new JsonArray();
+        for (String activity : board.getActivity()) {
+            activityArray.add(new JsonPrimitive(activity));
+        }
+        jsonObject.add("activityHistory", activityArray);
+
+        JsonArray tasksArray = new JsonArray();
+        for (int id : board.getTasks()) {
+            tasksArray.add(new JsonPrimitive(id));  // Corrected line
+        }
+        jsonObject.add("tasks", tasksArray);
+
+        return jsonObject;
+    }
 }
