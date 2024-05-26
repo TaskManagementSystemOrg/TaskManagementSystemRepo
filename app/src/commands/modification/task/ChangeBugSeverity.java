3938064e194aa3355d1cc2ca79114591386a4449
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-05-26 17:02:44 +0300

    Made commands work fixed a bug and made them follow the same logic as the others

--- app/src/commands/modification/task/ChangeBugSeverity.java
+++ app/src/commands/modification/task/ChangeBugSeverity.java
@@ -1,30 +1,76 @@
 package commands.modification.task;
 
+import Utils.ListingHelpers;
 import Utils.ParsingHelpers;
 import Utils.ValidationHelpers;
 import commands.contracts.Command;
 import core.contracts.TaskManagementSystemRepository;
 import models.contracts.Bug;
+import models.contracts.Task;
+import models.enums.Priority;
 import models.enums.Severity;
+import models.enums.TaskType;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ChangeBugSeverity implements Command {
     public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
-    protected final TaskManagementSystemRepository taskManagementSystemRepository;
+    protected final TaskManagementSystemRepository repository;
 
     public ChangeBugSeverity(TaskManagementSystemRepository taskManagementSystemRepository) {
-        this.taskManagementSystemRepository = taskManagementSystemRepository;
+        this.repository = taskManagementSystemRepository;
     }
 
     @Override
     public String execute(List<String> parameters) {
-        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
-        String bugName = parameters.get(0);
-        Severity severity = ParsingHelpers.tryParseEnum(parameters.get(1), Severity.class, "Not a valid input.");
-        Bug bug = (Bug) taskManagementSystemRepository.findTaskByName(bugName);
-        Severity oldValue = bug.getSeverity();
-        bug.setSeverity(severity);
-        return String.format("Severity changed from %s to %s", oldValue, severity);
+        Scanner scanner = new Scanner(System.in);
+        if (repository.getTasks().stream()
+                .noneMatch(task -> task.getType().equals(TaskType.BUG))) {
+            return "No bugs created yet.";
+        }
+        List<Bug> bugs = new ArrayList<>();
+        for (Task task : repository.getTasks()) {
+            if (task.getType().equals(TaskType.BUG)) {
+                bugs.add((Bug) task);
+            }
+        }
+
+
+        Bug bug = null;
+        System.out.println("Type help to see all options or enter a task: ");
+        String input = scanner.nextLine();
+
+        while (true) {
+            if (input.equalsIgnoreCase("help")) {
+                System.out.println("====================");
+                System.out.println(ListingHelpers.elementsToString(bugs));
+                System.out.println("====================");
+                System.out.println("Type help to see all options or enter task: ");
+                input = scanner.nextLine();
+            } else if (bugs.contains(repository.findTaskByName(input))) {
+                bug = (Bug) repository.findTaskByName(input);
+                break;
+            } else {
+                System.out.println("Not a valid input. Try again:");
+                input = scanner.nextLine();
+            }
+        }
+
+        System.out.print("Enter new severity(CRITICAL/MAJOR/MINOR): ");
+        input = scanner.nextLine();
+
+        while (true) {
+            if (input.equalsIgnoreCase("critical") || input.equalsIgnoreCase("major") || input.equalsIgnoreCase("minor")) {
+                bug.setSeverity(ParsingHelpers.tryParseEnum(input, Severity.class, "Invalid enum."));
+                return String.format("%s set to %s priority.", bug.getTitle(), bug.getSeverity());
+            } else {
+                System.out.println("Not a valid input. Try again:");
+                input = scanner.nextLine();
+            }
+        }
+
+
     }
 }
--- app/src/commands/modification/task/ChangeBugStatus.java
+++ app/src/commands/modification/task/ChangeBugStatus.java
@@ -1,30 +1,74 @@
 package commands.modification.task;
 
+import Utils.ListingHelpers;
 import Utils.ParsingHelpers;
 import Utils.ValidationHelpers;
 import commands.contracts.Command;
 import core.contracts.TaskManagementSystemRepository;
 import models.contracts.Bug;
+import models.contracts.Task;
 import models.enums.BugStatus;
+import models.enums.Severity;
+import models.enums.TaskType;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ChangeBugStatus implements Command {
-    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
-    protected final TaskManagementSystemRepository taskManagementSystemRepository;
+    protected final TaskManagementSystemRepository repository;
 
     public ChangeBugStatus(TaskManagementSystemRepository taskManagementSystemRepository) {
-        this.taskManagementSystemRepository = taskManagementSystemRepository;
+        this.repository = taskManagementSystemRepository;
     }
 
     @Override
     public String execute(List<String> parameters) {
-        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
-        String bugName = parameters.get(0);
-        BugStatus status = ParsingHelpers.tryParseEnum(parameters.get(1), BugStatus.class, "Not a valid input.");
-        Bug bug = (Bug) taskManagementSystemRepository.findTaskByName(bugName);
-        BugStatus oldValue = bug.getStatus();
-        bug.setStatus(status);
-        return String.format("Status changed from %s to %s", oldValue, status);
+        Scanner scanner = new Scanner(System.in);
+        if (repository.getTasks().stream()
+                .noneMatch(task -> task.getType().equals(TaskType.BUG))) {
+            return "No bugs created yet.";
+        }
+        List<Bug> bugs = new ArrayList<>();
+        for (Task task : repository.getTasks()) {
+            if (task.getType().equals(TaskType.BUG)) {
+                bugs.add((Bug) task);
+            }
+        }
+
+
+        Bug bug = null;
+        System.out.println("Type help to see all options or enter a task: ");
+        String input = scanner.nextLine();
+
+        while (true) {
+            if (input.equalsIgnoreCase("help")) {
+                System.out.println("====================");
+                System.out.println(ListingHelpers.elementsToString(bugs));
+                System.out.println("====================");
+                System.out.println("Type help to see all options or enter task: ");
+                input = scanner.nextLine();
+            } else if (bugs.contains(repository.findTaskByName(input))) {
+                bug = (Bug) repository.findTaskByName(input);
+                break;
+            } else {
+                System.out.println("Not a valid input. Try again:");
+                input = scanner.nextLine();
+            }
+        }
+
+        System.out.print("Enter new status(ACTIVE/DONE): ");
+        input = scanner.nextLine();
+
+        while (true) {
+            if (input.equalsIgnoreCase("Done") || input.equalsIgnoreCase("Active")) {
+                bug.setStatus(ParsingHelpers.tryParseEnum(input, BugStatus.class, "Invalid enum."));
+                return String.format("%s set to %s priority.", bug.getTitle(), bug.getStatus());
+            } else {
+                System.out.println("Not a valid input. Try again:");
+                input = scanner.nextLine();
+            }
+        }
+
     }
 }
